fd5b11c8e248d9abc35011211e7e8a2a6fff3f61
Allyson Halley <allysonhalley@gmail.com>
2021-07-30 03:11:41 -0300

    remake with h2

--- src/main/java/com/example/devsuperior/resources/CategoryResource.java
+++ src/main/java/com/example/devsuperior/resources/CategoryResource.java
@@ -1,13 +1,16 @@
 package com.example.devsuperior.resources;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.devsuperior.entities.Category;
@@ -31,9 +34,17 @@ public class CategoryResource {
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Category> findById(@PathVariable Long id){
 		
-		Category cat = categoryRepository.findById(id);
+		Category cat = categoryRepository.findById(id).get();
 		
 		return ResponseEntity.ok().body(cat);
+	}
+	
+	@PostMapping(path = "/category", consumes = "application/json")
+	@ResponseStatus(HttpStatus.CREATED)
+	public void addCategory(@RequestBody Category category) {
+		
+		categoryRepository.save(category);
+		
 	}
 	
 	
--- src/main/java/com/example/devsuperior/resources/ProductResource.java
+++ src/main/java/com/example/devsuperior/resources/ProductResource.java
@@ -3,10 +3,14 @@ package com.example.devsuperior.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.devsuperior.entities.Product;
@@ -30,9 +34,15 @@ public class ProductResource {
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Product> findById(@PathVariable Long id){
 		
-		Product prod = productRepository.findById(id);
+		Product prod = productRepository.findById(id).get();
 		
 		return ResponseEntity.ok().body(prod);
+	}
+	
+	@PostMapping(path = "/product", consumes = "application/json")
+	@ResponseStatus(HttpStatus.CREATED)
+	public void addProduct(@RequestBody Product product) {
+		productRepository.save(product);
 	}
 	
 	
